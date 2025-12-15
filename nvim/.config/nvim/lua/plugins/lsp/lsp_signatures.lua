18ff7f23a646e111f1c3121e971347bfcb8b9eae
FelixHommel <flixh10@gmail.com>
2025-12-15 01:32:58 +0100

    refactor: clean config for lsp_signatures

--- nvim/.config/nvim/lua/plugins/lsp/lsp_signatures.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lsp_signatures.lua
@@ -1,16 +1,16 @@
 return {
     "ray-x/lsp_signature.nvim",
-    event = "VeryLazy",
+    event = "InsertEnter",
     dependencies = {
         "folke/which-key.nvim"
     },
     opts = {
-        hint_prefix = "",
-        transparency = 10,
-        toggle_key = "<M-l>",
-        select_signature_key = "<M-e>",
+        bind = true,
+        wrap = true,
+        hint_enable = false,
+        transparency = 5,
         handler_opts = {
-            border = "single"
+            border = "rounded"
         }
     }
 }
