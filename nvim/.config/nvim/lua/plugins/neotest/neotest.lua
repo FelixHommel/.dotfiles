1eff0070f8287e591d8c72399302ee11f2dcca79
FelixHommel <felixhommel04@gmail.com>
2026-01-20 11:17:56 +0100

    refactor: reformat the keybinds

--- nvim/.config/nvim/lua/plugins/neotest/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest/neotest.lua
@@ -9,34 +9,42 @@ return {
         "alfaix/neotest-gtest",
         "orjangj/neotest-ctest"
     },
-    keys = function()
-        local neotest = require("neotest")
-
-        return {
-            { "<leader>ta", function() neotest.run.run(vim.fn.expand("%")) end, desc = "Run File" },
-            { "<leader>tt", function() neotest.run.run() end, desc = "Run Nearest" },
-            { "<leader>tw", function() neotest.run.run(vim.loop.cwd()) end, desc = "Run Workspace" },
-            {
-                "<leader>tr",
-                function()
-                    -- This will only show the output from the test framework
-                    neotest.output.open({ short = true, auto_close = true })
-                end,
-                desc = "Results (short)",
-            },
-            {
-                "<leader>tR",
-                function()
-                    -- This will show the classic CTest log output.
-                    -- The output usually spans more than can fit the neotest floating window,
-                    -- so using 'enter = true' to enable normal navigation within the window
-                    -- is recommended.
-                    neotest.output.open({ enter = true })
-                end,
-                desc = "Results (full)",
-            }
+    keys =  {
+        {
+            "<leader>ta",
+            function() require("neotest").run.run(vim.fn.expand("%")) end,
+            desc = "Run File"
+        },
+        {
+            "<leader>tt",
+            function() require("neotest").run.run() end,
+            desc = "Run Nearest"
+        },
+        {
+            "<leader>tw",
+            function() require("neotest").run.run(vim.loop.cwd()) end,
+            desc = "Run Workspace"
+        },
+        {
+            "<leader>tr",
+            function()
+                -- This will only show the output from the test framework
+                require("neotest").output.open({ short = true, auto_close = true })
+            end,
+            desc = "Results (short)",
+        },
+        {
+            "<leader>tR",
+            function()
+                -- This will show the classic CTest log output.
+                -- The output usually spans more than can fit the require("neotest") floating window,
+                -- so using 'enter = true' to enable normal navigation within the window
+                -- is recommended.
+                require("neotest").output.open({ enter = true })
+            end,
+            desc = "Results (full)",
         }
-    end,
+    },
     config = function()
         require("neotest").setup({
             adapters = {
