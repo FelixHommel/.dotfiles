ab5f664bf4b0c3e670ab217448f27cc75c41c73a
FelixHommel <flixh10@gmail.com>
2026-05-06 13:25:23 +0200

    refactor(nvim): update snippets

--- nvim/.config/nvim/lua/plugins/lsp/luaSnip.lua
+++ nvim/.config/nvim/lua/plugins/lsp/luaSnip.lua
@@ -11,16 +11,16 @@ require("luasnip").add_snippets("cpp", {
 
         class {}
         {{
-            public:
-                {}();
-                ~{}() = default;
+        public:
+            {}();
+            ~{}() = default;
 
-                {}(const {}&) = delete;
-                {}({}&&) = delete;
-                {}& operator=(const {}&) = delete;
-                {}& operator=({}&&) = delete;
+            {}(const {}&) = delete;
+            {}& operator=(const {}&) = delete;
+            {}({}&&) noexcept = delete;
+            {}& operator=({}&&) noexcept = delete;
 
-            private:
+        private:
         }};
 
         #endif //!{}
@@ -39,22 +39,40 @@ require("luasnip").add_snippets("cpp", {
 
         class {}
         {{
-            public:
-                {}();
-                ~{}() = default;
+        public:
+            {}();
+            ~{}() = default;
 
-                {}(const {}&) = delete;
-                {}({}&&) = delete;
-                {}& operator=(const {}&) = delete;
-                {}& operator=({}&&) = delete;
+            {}(const {}&) = delete;
+            {}& operator=(const {}&) = delete;
+            {}({}&&) noexcept = delete;
+            {}& operator=({}&&) noexcept = delete;
 
-            private:
+        private:
         }};
 
         }} //!{}
 
         #endif //!{}
         ]], {i(1), r(1), i(2), i(3), r(3), r(3), r(3), r(3), r(3), r(3), r(3), r(3), r(3), r(3), r(2), r(1)}
+    ))
+})
+
+require("luasnip").add_snippets("cmake", {
+    s("init_project", f(
+        [[
+        cmake_minimum_required(VERSION 3.30)
+        project({} LANGUAGES CXX)
+
+        set(CMAKE_CXX_STANDARD 23)
+        set(CMAKE_CXX_STANDARD_REQUIRED ON)
+        set(CMAKE_CXX_EXTENSIONS OFF)
+        set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+        add_executable(${{PROJECT_NAME}} ./main.cpp)
+
+        target_compile_features(${{PROJECT_NAME}} PRIVATE cxx_std_23)
+        ]], {i(1)}
     ))
 })
 
